e08fa6678b5c2a1a5565fd5dad971f376c9a5aba
Ricardo Bozon <114138253+ricardobozondesarrollador@users.noreply.github.com>
2022-11-07 23:43:40 -0500

    aside carrito

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
 const menuHamIcon=document.querySelector('.menu');
+const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 const mobileMenu=document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu (){
   desktopMenu.classList.toggle('inactive');      
 
 }
 
+
 function toggleMobileMenu (){
   mobileMenu.classList.toggle('inactive');      
+
+}
+
+function toggleCarritoAside (){
+  aside.classList.toggle('inactive');      
 
 }
